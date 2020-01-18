9c3a7abcd13ab64f76530618fc7cce64c5a89365
roman.prokashev <roman.prokashev@travelline.ru>
2020-01-18 17:01:54 +0300

    Проверка работы

---
+++ Search.js
@@ -0,0 +1,33 @@
+class Search extends Component {
+    constructor() {
+        super();
+        this._onChange = null;
+        this._onSearchChange = this._onSearchChange.bind(this);
+    }
+
+    get template() {
+        return `<input type="text" name="search" placeholder="Search">
+      <button type="submit" class="visually-hidden">Search</button>`.trim();
+    }
+
+    removeEventListener() {
+        this._element
+            .removeEventListener(`keydown`, this._onSearchChange);
+    }
+
+    _onSearchChange(event) {
+        if (typeof this._onChange === `function`) {
+            this._onChange(event.target.value);
+        }
+    }
+
+    set onChange(fn) {
+        this._onChange = fn;
+    }
+
+    setEventListener() {
+        this._element
+            .addEventListener(`keyup`, this._onSearchChange);
+    }
+
+}
